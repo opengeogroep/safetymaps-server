704e1621013cec578d00256ca39649bae9320e6a
Meine Toonen <toonen.meine@gmail.com>
2014-06-27 08:40:54 +0200

    First import of the backend of the DBKComponent in Flamingo4

---
+++ src/main/java/nl/b3p/dbk/DBKAPI.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2014 B3Partners B.V.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.dbk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Meine Toonen
+ */
+public class DBKAPI extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Features</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Features at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            processFeature();
+        }
+    }
+
+    private void processFeature() throws SQLException {
+        Connection conn = getConnection();
+        try {
+            // make sure autocommit is off
+            conn.setAutoCommit(false);
+            Statement st = conn.createStatement();
+
+// Turn use of the cursor on.
+            st.setFetchSize(50);
+            ResultSet rs = st.executeQuery("select \"feature\" from dbk.dbkfeatures_json()");
+            
+            while (rs.next()) {
+                int i = 0;
+                System.out.print("a row was returned.");
+            }
+            rs.close();
+
+// Close the statement.
+            st.close();
+        } catch (SQLException ex) {
+            conn.close();
+        }
+    }
+
+    private void processObject() {
+
+    }
+
+    public Connection getConnection() throws SQLException {
+        Properties props = new Properties();
+        props.put("user", "dbk");
+        props.put("password", "***");
+        return DriverManager.getConnection("jdbc:postgresql://vru/dbk", props);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBKAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBKAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
