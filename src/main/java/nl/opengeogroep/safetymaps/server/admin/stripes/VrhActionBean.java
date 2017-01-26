510b8189c27bded8163ff7112c51aa985ed6cf34
Matthijs Laan <matthijsln@gmail.com>
2017-01-26 11:15:54 +0100

    update voor nieuwe shapefiles

--- src/main/java/nl/opengeogroep/safetymaps/server/admin/stripes/VrhActionBean.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/admin/stripes/VrhActionBean.java
@@ -72,25 +72,9 @@ public class VrhActionBean implements ActionBean {
         JSONObject result = new JSONObject();
         result.put("success", false);
         try {
-            List<Map<String,Object>> rows = DB.qr().query("select id, locatie, adres, plaatsnaam, st_xmin(geom)||','||st_ymin(geom)||','||st_xmax(geom)||','||st_ymax(geom) as bounds, st_asgeojson(st_centroid(geom)) as geometry from vrh.wbbk where locatie is not null order by locatie", new MapListHandler());
+            List<Map<String,Object>> rows = DB.qr().query("select id, locatie, adres, plaatsnaam, st_xmin(geom)||','||st_ymin(geom)||','||st_xmax(geom)||','||st_ymax(geom) as bounds, st_asgeojson(st_centroid(geom)) as geometry from vrh.wdbk_waterbereikbaarheidskaart where locatie is not null order by locatie", new MapListHandler());
 
             result.put("wbbk", rowsToGeoJSONFeatureCollection(rows));
-/*            JSONArray features = new JSONArray();
-            //result.put("wbbk", features);
-
-            for(Map<String,Object> row: rows) {
-                JSONObject feature = new JSONObject();
-                features.put(feature);
-                for(Map.Entry<String,Object> column: row.entrySet()) {
-                    if("geometry".equals(column.getKey()) || "centroid".equals(column.getKey())) {
-                        JSONObject geometry = new JSONObject((String)column.getValue());
-                        feature.put(column.getKey(), geometry);
-                    } else if(!"geom".equals(column.getKey())) {
-                        feature.put(column.getKey(), column.getValue());
-                    }
-                }
-            }*/
-
             result.put("success", true);
         } catch(Exception e) {
             log.error("Error getting VRH wbbk data", e);
@@ -105,7 +89,7 @@ public class VrhActionBean implements ActionBean {
         JSONObject result = new JSONObject();
         result.put("success", false);
         try {
-            List<Map<String,Object>> rows = DB.qr().query("select *, st_asgeojson(geom) as geometry from vrh.wbbk where id = ?", new MapListHandler(), id);
+            List<Map<String,Object>> rows = DB.qr().query("select *, st_asgeojson(geom) as geometry from vrh.wdbk_waterbereikbaarheidskaart where id = ?", new MapListHandler(), id);
             if(rows.isEmpty()) {
                 result.put("error", "WBBK met ID " + id + " niet gevonden");
             } else {
@@ -128,13 +112,13 @@ public class VrhActionBean implements ActionBean {
                     }
                 }
 
-                rows = DB.qr().query("select id, symboolcod, symboolgro, bijzonderh, st_asgeojson(geom) as geometry from vrh.symbolen where dbk_object = ?", new MapListHandler(), id);
+                rows = DB.qr().query("select id, symboolcod, symboolgro, bijzonderh, st_asgeojson(geom) as geometry from vrh.voorzieningen_water where dbk_object = ?", new MapListHandler(), id);
                 result.put("symbolen", rowsToGeoJSONFeatureCollection(rows));
 
-                rows = DB.qr().query("select id, type, bijzonderh, opmerkinge, st_asgeojson(geom) as geometry from vrh.lijnen where dbk_object = ?", new MapListHandler(), id);
+                rows = DB.qr().query("select id, type, bijzonderh, opmerkinge, st_asgeojson(geom) as geometry from vrh.overige_lijnen where dbk_object = ?", new MapListHandler(), id);
                 result.put("lijnen", rowsToGeoJSONFeatureCollection(rows));
 
-                rows = DB.qr().query("select id, type, bijzonderh, opmerkinge, st_asgeojson(geom) as geometry from vrh.vlakken where dbk_object = ? order by \n" +
+                rows = DB.qr().query("select id, type, bijzonderh, st_asgeojson(geom) as geometry from vrh.overige_vlakken where dbk_object = ? order by \n" +
                     "   case type \n" +
                     "   when 'Dieptevlak 4 tot 9 meter' then -3 \n" +
                     "   when 'Dieptevlak 9 tot 15 meter' then -2\n" +
