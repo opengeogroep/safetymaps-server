ac4b22ae18bd8bea2cbc3283d268c86feb2eea8d
Matthijs Laan <matthijsln@gmail.com>
2018-02-21 19:16:02 +0100

    fix weird error column geom does not exist; get diameter from closest dunea leiding

--- src/main/java/nl/opengeogroep/safetymaps/server/stripes/VrhWaterwinningApiActionBean.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/stripes/VrhWaterwinningApiActionBean.java
@@ -9,6 +9,7 @@ import net.sourceforge.stripes.validation.Validate;
 import nl.opengeogroep.safetymaps.server.db.DB;
 import static nl.opengeogroep.safetymaps.server.db.JsonExceptionUtils.logExceptionAndReturnJSONObject;
 import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
@@ -156,7 +157,7 @@ public class VrhWaterwinningApiActionBean implements ActionBean {
         dist = Math.min(dist, MAX_DISTANCE);
         count = Math.min(count, MAX_COUNT);
 
-        List<Map<String,Object>> rows = DB.qr().query("select st_distance(geom, st_setsrid(st_point(?, ?),?)) as distance, st_x(geom) as x, st_y(geom) as y, * "
+        List<Map<String,Object>> rows = DB.qr().query("select st_distance(b.geom, st_setsrid(st_point(?, ?),?)) as distance, st_x(geom) as x, st_y(geom) as y, * "
                 + "from "
                 + " (select geom, 'brandkranen_eigen_terrein' as tabel, \"type\", 'NB' as info from vrh.brandkranen_eigen_terrein "
                 + "  union all "
@@ -165,7 +166,7 @@ public class VrhWaterwinningApiActionBean implements ActionBean {
                 + "  select geom, 'brandkranen_evides' as tabel, 'ondergronds' as \"type\", 'Nominale druk: ' || nominale_d as info from vrh.brandkranen_evides "
                 + "  union all "
                 + "  select geom, 'brandkranen_oasen' as tabel, case when lower(ondergrnds) = 'nee' then 'bovengronds' else 'ondergronds' end as \"type\", omschr_lok as info from vrh.brandkranen_oasen) b "
-                + "where st_distance(geom, st_setsrid(st_point(?, ?),?)) < ? "
+                + "where st_distance(b.geom, st_setsrid(st_point(?, ?),?)) < ? "
                 + "order by 1 asc limit ?", new MapListHandler(), x, y, srid, x, y, srid, dist, count);
         
         log.info("Waterwinning results " + getContext().getRequest().getRequestURI() + "?" + getContext().getRequest().getQueryString() + ": " + rows);
@@ -176,6 +177,21 @@ public class VrhWaterwinningApiActionBean implements ActionBean {
                 if(!"geom".equals(key)) {
                     o.put(key, row.get(key));
                 }
+            }
+            if("brandkranen_dunea".equals(row.get("tabel"))) {
+                Integer diameter = DB.qr().query("select artikel_d2 from vrh.hoofdleidingen_dunea order by st_distance(geom, st_setsrid(st_point(?, ?),?)) limit 1", new ScalarHandler<Integer>(), row.get("x"), row.get("y"), srid);
+                log.info(String.format("Leiding voor Dunea brandkraan op %s, %s is %s", row.get("x"), row.get("y"), diameter));
+                int opbrengst = 500;
+                if(diameter > 70) {
+                    opbrengst = 1000;
+                }
+                if(diameter > 120) {
+                    opbrengst = 1500;
+                }
+                if(diameter > 160) {
+                    opbrengst = 2000;
+                }
+                o.put("info", opbrengst + " &#8467;/min (diameter leiding: " + diameter + " mm)");
             }
             ww.put(o);
         }
