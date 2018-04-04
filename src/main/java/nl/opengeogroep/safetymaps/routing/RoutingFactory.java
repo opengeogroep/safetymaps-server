ea84eee23daf1bd347e92ab152b309b8e4c09253
Matthijs Laan <matthijsln@gmail.com>
2018-04-04 18:44:11 +0200

    added GraphHopper routing engine

--- src/main/java/nl/opengeogroep/safetymaps/routing/RoutingFactory.java
+++ src/main/java/nl/opengeogroep/safetymaps/routing/RoutingFactory.java
@@ -5,21 +5,39 @@
  */
 package nl.opengeogroep.safetymaps.routing;
 
+import nl.opengeogroep.safetymaps.routing.service.GraphHopper;
 import nl.opengeogroep.safetymaps.routing.service.OpenRouteService;
 import nl.opengeogroep.safetymaps.server.db.Cfg;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  *
  * @author matthijsln
  */
 public class RoutingFactory {
+    private static final Log log = LogFactory.getLog(RoutingFactory.class);
+
     public static RoutingService getRoutingService() throws Exception {
         try {
-            String apiKey = Cfg.getSetting("openrouteservice_apikey");
-            OpenRouteService service = new OpenRouteService(apiKey);
-            service.setProfile(Cfg.getSetting("openrouteservice_profile"));
-            service.setPreference(Cfg.getSetting("openrouteservice_preference"));
-            return service;
+            String engine = Cfg.getSetting("routing_engine", "openrouteservice");
+
+            if("openrouteservice".equals(engine)) {
+                String apiKey = Cfg.getSetting("openrouteservice_apikey");
+                OpenRouteService service = new OpenRouteService(apiKey);
+                service.setProfile(Cfg.getSetting("openrouteservice_profile"));
+                service.setPreference(Cfg.getSetting("openrouteservice_preference"));
+                log.info("Using OpenRouteService engine for routing");
+                return service;
+            } else if("graphhopper".equals(engine)) {
+                String url = Cfg.getSetting("graphhopper_url");
+                GraphHopper service = new GraphHopper();
+                service.setURL(url);
+                log.info("Using GraphHopper engine for routing with URL " + url);
+                return service;
+            } else {
+                throw new IllegalArgumentException("Invalid routing engine configured: " + engine);
+            }
         } catch(Exception e) {
             throw new Exception("Error initializing routing engine: " + e.getMessage(), e);
         }
---
+++ src/main/java/nl/opengeogroep/safetymaps/routing/service/GraphHopper.java
@@ -0,0 +1,147 @@
+package nl.opengeogroep.safetymaps.routing.service;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import nl.opengeogroep.safetymaps.routing.RoutingException;
+import nl.opengeogroep.safetymaps.routing.RoutingRequest;
+import nl.opengeogroep.safetymaps.routing.RoutingResult;
+import nl.opengeogroep.safetymaps.routing.RoutingService;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpResponse;
+import static org.apache.http.HttpStatus.SC_OK;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.geotools.geometry.jts.JTS;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.referencing.CRS;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+
+/**
+ *
+ * @author matthijsln
+ */
+public class GraphHopper implements RoutingService {
+    private static final Log log = LogFactory.getLog(GraphHopper.class);
+
+    private String URL = "http://localhost:11111/route";
+    
+    public GraphHopper() {
+    
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+    
+    @Override
+    public RoutingResult getRoute(RoutingRequest request) throws RoutingException {
+        RoutingResult result;
+        try {
+            CoordinateReferenceSystem crs = CRS.decode("epsg:28992");
+            CoordinateReferenceSystem serviceCrs = CRS.decode("epsg:4326");
+            MathTransform transform = CRS.findMathTransform(crs, serviceCrs, true);
+            
+            GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory( null );
+            Coordinate coord = new Coordinate(request.getFromX(), request.getFromY());
+            Point from = geometryFactory.createPoint(coord);
+            coord = new Coordinate(request.getToX(), request.getToY());
+            Point to = geometryFactory.createPoint(coord);
+
+            Point fromTransformed = (Point)JTS.transform(from, transform);
+            Point toTransformed = (Point)JTS.transform(to, transform);
+                    
+            log.info("Reprojected destination point to service CRS: " + toTransformed);
+            
+            try(CloseableHttpClient client = getClient()) {
+                HttpUriRequest get = RequestBuilder.get()
+                        .setUri(URL)
+                        .addHeader("Accept", "text/json; charset=utf-8, application/json")
+                        .addParameter("point", fromTransformed.getX() + "," + fromTransformed.getY())
+                        .addParameter("point", toTransformed.getX() + "," + toTransformed.getY())
+                        .addParameter("vehicle", "car")
+                        .addParameter("weighting", "fastest")
+                        .addParameter("locale", "nl")
+                        .addParameter("type", "json")
+                        .addParameter("elevation", "false")
+                        .addParameter("points_encoded", "false")
+                        .build();
+
+                log.info("GET > " + get.getRequestLine());
+                String response = client.execute(get, new ResponseHandler<String>() {
+                    @Override
+                    public String handleResponse(HttpResponse hr) {
+                        log.trace("< " + hr.getStatusLine());
+                        String entity = null;
+                        try {
+                            entity = IOUtils.toString(hr.getEntity().getContent(), "UTF-8");
+                        } catch(IOException e) {
+                        }
+                        if(hr.getStatusLine().getStatusCode() != SC_OK) {
+                            log.error("HTTP error: " + hr.getStatusLine() + ", " + entity);
+                        }
+                        log.trace("< entity: " + entity);
+                        return entity;
+                    }
+                });
+                if(response != null) {
+                    JSONObject res = new JSONObject(response);
+                    log.info("JSON response: " + res.toString(4));
+                    
+                    if(res.has("message")) {
+                        result = new RoutingResult(false, "GraphHopper routing engine error: " + res.getString("message"), null);
+                    } else {
+                        result = new RoutingResult(true, res);
+                        result.setDistance(calculateDistance(res));
+                    }
+                    result.setRoute(res);
+                } else {
+                    result = new RoutingResult(false, "Error calculating route: null response", null);
+                }
+            }
+
+        } catch(Exception e) {
+            log.error("Error calculating route", e);
+            result = new RoutingResult(false, "Error calculating route: " + e.getMessage(), e);
+        }    
+        return result;
+    }
+    
+    private static double calculateDistance(JSONObject route) {
+        
+        double distance = 0;
+        JSONArray paths = route.getJSONArray("paths");
+        for(int i = 0; i < paths.length(); i++) {
+            distance += paths.getJSONObject(i).getDouble("distance");
+        }
+        return distance;
+    }
+    
+    private static CloseableHttpClient getClient() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        return HttpClients.custom()
+            .setDefaultRequestConfig(RequestConfig.custom()
+                    .setConnectTimeout(5 * 1000)
+                    .setSocketTimeout(10 * 1000)
+                    .build()
+            )
+            .build();
+    }
+    
+}
