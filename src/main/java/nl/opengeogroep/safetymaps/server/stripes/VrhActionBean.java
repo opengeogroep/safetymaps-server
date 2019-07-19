2beb4b514d8c91797e8df32c3abfced182cfbbae
Matthijs Laan <matthijsln@gmail.com>
2019-07-19 19:12:28 +0200

    add vrh newDbSchema option

--- src/main/java/nl/opengeogroep/safetymaps/server/stripes/VrhActionBean.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/stripes/VrhActionBean.java
@@ -23,9 +23,12 @@ import nl.opengeogroep.safetymaps.server.db.JSONUtils;
 import static nl.opengeogroep.safetymaps.server.db.JSONUtils.rowToJson;
 import static nl.opengeogroep.safetymaps.server.db.JsonExceptionUtils.logExceptionAndReturnJSONObject;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.KeyedHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
@@ -55,6 +58,9 @@ public class VrhActionBean implements ActionBean {
     @Validate
     private int indent = 0;
 
+    @Validate
+    private boolean newDbSchema = false;
+
     // <editor-fold defaultstate="collapsed" desc="getters and setters">
     @Override
     public ActionBeanContext getContext() {
@@ -81,6 +87,14 @@ public class VrhActionBean implements ActionBean {
     public void setIndent(int indent) {
         this.indent = indent;
     }
+
+    public boolean isNewDbSchema() {
+        return newDbSchema;
+    }
+
+    public void setNewDbSchema(boolean newDbSchema) {
+        this.newDbSchema = newDbSchema;
+    }
     // </editor-fold>
 
     private static JSONArray rowsToJSONArray(List<Map<String,Object>> rows) throws Exception {
@@ -89,6 +103,32 @@ public class VrhActionBean implements ActionBean {
             a.put(JSONUtils.rowToJson(row, true, true));
         }
         return a;
+    }
+
+    private Resolution jsonStreamingResolution(final JSONObject j, final Long lastModified) {
+        return new Resolution() {
+            @Override
+            public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+                String encoding = "UTF-8";
+                response.setCharacterEncoding(encoding);
+                response.setContentType("application/json");
+                if(lastModified != null) {
+                    response.addDateHeader("Last-Modified", lastModified * 1000);
+                }
+
+                OutputStream out;
+                String acceptEncoding = request.getHeader("Accept-Encoding");
+                if(acceptEncoding != null && acceptEncoding.contains("gzip")) {
+                    response.setHeader("Content-Encoding", "gzip");
+                    out = new GZIPOutputStream(response.getOutputStream(), true);
+                } else {
+                    out = response.getOutputStream();
+                }
+                IOUtils.copy(new StringReader(j.toString(indent)), out, encoding);
+                out.flush();
+                out.close();
+            }
+        };
     }
 
     private int getIdFromPath(String type) throws Exception {
@@ -113,7 +153,11 @@ public class VrhActionBean implements ActionBean {
                 if(MAP_DBKS.equals(path)) {
                     return dbks(c);
                 } else if(path.indexOf(TYPE_DBK + "/") == 0) {
-                    result = dbkJson(c, getIdFromPath(TYPE_DBK));
+                    if(newDbSchema) {
+                        result = dbkJsonNewDbSchema(c, getIdFromPath(TYPE_DBK));
+                    } else {
+                        result = dbkJson(c, getIdFromPath(TYPE_DBK));
+                    }
                 } else if(MAP_WATERONGEVALLEN.equals(path)) {
                     result = waterongevallenJson(c);
                 } else if(path.indexOf(TYPE_WATERONGEVALLEN + "/") == 0) {
@@ -129,32 +173,33 @@ public class VrhActionBean implements ActionBean {
                 final JSONObject r = new JSONObject();
                 r.put("success", true);
                 r.put("results", result);
-                return new Resolution() {
-                    @Override
-                    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-                        String encoding = "UTF-8";
-                        response.setCharacterEncoding(encoding);
-                        response.setContentType("application/json");
-
-                        OutputStream out;
-                        String acceptEncoding = request.getHeader("Accept-Encoding");
-                        if(acceptEncoding != null && acceptEncoding.contains("gzip")) {
-                            response.setHeader("Content-Encoding", "gzip");
-                            out = new GZIPOutputStream(response.getOutputStream(), true);
-                        } else {
-                            out = response.getOutputStream();
-                        }
-                        IOUtils.copy(new StringReader(r.toString(indent)), out, encoding);
-                        out.flush();
-                        out.close();
-                    }
-                };
+                return jsonStreamingResolution(r, null);
             }
 
             return new ErrorResolution(HttpServletResponse.SC_NOT_FOUND, "Not found: " + getContext().getRequest().getRequestURI());
         } catch(Exception e) {
             return new StreamingResolution("application/json", logExceptionAndReturnJSONObject(log, "Error on " + getContext().getRequest().getRequestURI(), e).toString(indent));
         }
+    }
+
+    private Resolution dbks(Connection c) throws Exception {
+
+        if(newDbSchema) {
+            return dbksNewDbSchema(c);
+        }
+
+        final long lastModified = new QueryRunner().query(c, "select time from vrh.import_metadata limit 1", new ScalarHandler<Timestamp>()).getTime() / 1000;
+        long ifModifiedSince = getContext().getRequest().getDateHeader("If-Modified-Since") / 1000;
+
+        if(ifModifiedSince >= lastModified) {
+            return new ErrorResolution(HttpServletResponse.SC_NOT_MODIFIED);
+        }
+
+        final JSONObject r = new JSONObject();
+        r.put("success", true);
+        r.put("results", dbksJson(c));
+
+        return jsonStreamingResolution(r, lastModified);
     }
 
     public static JSONArray dbksJson(Connection c) throws Exception {
@@ -205,8 +250,8 @@ public class VrhActionBean implements ActionBean {
         return objects;
     }
 
-    private Resolution dbks(Connection c) throws Exception {
-        final long lastModified = new QueryRunner().query(c, "select time from vrh.import_metadata limit 1", new ScalarHandler<Timestamp>()).getTime() / 1000;
+    private Resolution dbksNewDbSchema(Connection c) throws Exception {
+        final long lastModified = new QueryRunner().query(c, "select time from vrh_new.import_metadata limit 1", new ScalarHandler<Timestamp>()).getTime() / 1000;
         long ifModifiedSince = getContext().getRequest().getDateHeader("If-Modified-Since") / 1000;
 
         if(ifModifiedSince >= lastModified) {
@@ -215,29 +260,185 @@ public class VrhActionBean implements ActionBean {
 
         final JSONObject r = new JSONObject();
         r.put("success", true);
-        r.put("results", dbksJson(c));
 
-        return new Resolution() {
-            @Override
-            public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-                String encoding = "UTF-8";
-                response.setCharacterEncoding(encoding);
-                response.setContentType("application/json");
-                response.addDateHeader("Last-Modified", lastModified * 1000);
+        QueryRunner qr = new QueryRunner();
+        JSONArray objects = new JSONArray();
 
-                OutputStream out;
-                String acceptEncoding = request.getHeader("Accept-Encoding");
-                if(acceptEncoding != null && acceptEncoding.contains("gzip")) {
-                    response.setHeader("Content-Encoding", "gzip");
-                    out = new GZIPOutputStream(response.getOutputStream(), true);
-                } else {
-                    out = response.getOutputStream();
-                }
-                IOUtils.copy(new StringReader(r.toString(indent)), out, encoding);
-                out.flush();
-                out.close();
+        List<Map<String,Object>> rows = qr.query(c, "select dbk_object as id, naam_pand as naam, oms_nummer, vrh_bag_id, " +
+                "st_astext(st_centroid(geom)) as pand_centroid, box2d(geom)::varchar as extent " +
+                "from vrh_new.vrh_geo_pand " +
+                "where hoofd_sub='Hoofdpand' " +
+                "and naam_pand is not null " +
+                "order by naam_pand", new MapListHandler());
+
+        Set<String> nummeraanduidingIds = new HashSet();
+
+        for(Map<String,Object> row: rows) {
+            String vrhBagId = (String)row.get("vrh_bag_id");
+            if(vrhBagId != null) {
+                nummeraanduidingIds.add(vrhBagId);
             }
-        };
+        }
+
+        Map<String,Map<String,Object>> adresRows = (Map<String,Map<String,Object>>)DB.bagQr().query("select nummeraanduiding, openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam, st_astext(st_force2d(geopunt)) as geopunt from bag_actueel.adres where nummeraanduiding in (" + StringUtils.repeat("?", ", ", nummeraanduidingIds.size()) + ")", new  KeyedHandler("nummeraanduiding"), (Object[])nummeraanduidingIds.toArray(new String[] {}));
+
+        Set objectIds = new HashSet();
+
+        for(Map<String,Object> row: rows) {
+            JSONObject o = JSONUtils.rowToJson(row, true, true);
+
+            Object id = row.get("id");
+            if(objectIds.contains(id)) {
+                // Duplicate hoofdpand pand row
+                continue;
+            }
+            objectIds.add(id);
+
+            String vrhBagId = (String)row.get("vrh_bag_id");
+            Map<String,Object> bagAdres = adresRows.get(vrhBagId);
+
+            if(bagAdres != null) {
+                o.put("straatnaam", bagAdres.get("openbareruimtenaam"));
+                o.put("huisnummer", bagAdres.get("huisnummer"));
+                o.put("huisletter", bagAdres.get("huisletter"));
+                o.put("toevoeging", bagAdres.get("huisnummertoevoeging"));
+                o.put("postcode", bagAdres.get("postcode"));
+                o.put("plaats", bagAdres.get("woonplaatsnaam"));
+                o.put("bagPunt", bagAdres.get("geopunt"));
+            }
+            
+            objects.put(o);
+        }
+
+        r.put("results", objects);
+
+        return jsonStreamingResolution(r, lastModified);
+    }
+
+    public static JSONObject dbkJsonNewDbSchema(Connection c, int id) throws Exception {
+        List<Map<String,Object>> rows = new QueryRunner().query(c, "select " +
+                "    o.*, " +
+                "    (select st_astext(st_collect(sp.geom)) from vrh_new.vrh_geo_pand sp where sp.dbk_object = p.dbk_object) as geometry, " +
+//                "    st_astext(p.geom) as geometry, " +
+
+                "    (select row_to_json(r.*) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_pand t " +
+                "         where t.dbk_object = o.id" +
+                "         and hoofd_sub = 'Hoofdpand'" +
+                "         limit 1) r " +
+                "    ) as hoofdpand, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_pand t " +
+                "         where t.dbk_object = o.id" +
+                "         and hoofd_sub = 'Subpand') r " +
+                "    ) as subpanden, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_compartimentering t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as compartimentering, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, symboolcod, symboolhoe, symboolgro, omschrijvi, bijzonderh, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_brandweervoorziening t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as brandweervoorziening, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_opstelplaats t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as opstelplaats, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, symboolcod, symboolgro, omschrijvi, bijzonderh, symboolhoe, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_toegang_pand t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as toegang_pand, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_toegang_terrein t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as toegang_terrein, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, symboolcod, symboolgro, bijzonderh, soort_geva, locatie, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_gevaren t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as gevaren, " +
+/*
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh.hellingbaan t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as hellingbaan, " +
+*/
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_gevaarlijke_stoffen t " +
+                "         where t.dbk_object = o.id " +
+                "         order by volgnummer) r " +
+                "    ) as gevaarlijke_stoffen, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, type, bijzonderh, opmerkinge, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_dbk_lijn t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as overige_lijnen, " +
+/*
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh.slagboom t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as slagboom, " +
+
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh.aanpijling t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as aanpijling, " +
+*/
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select objectid, tekst, symboolgro, symboolhoe, st_astext(t.geom) as geometry " +
+                "         from vrh_new.vrh_geo_tekst t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as teksten " +
+/*
+                "    (select array_to_json(array_agg(row_to_json(r.*))) " +
+                "    from (select *, st_astext(t.geom) as geometry " +
+                "         from vrh.aanrijroute t " +
+                "         where t.dbk_object = o.id) r " +
+                "    ) as aanrijroute " +
+*/
+                "from vrh_new.vrh_geo_pand p " +
+                "left join vrh_new.vrh_geo_dbk_bag_object o on (o.vrh_bag_id = p.vrh_bag_id) " +
+                "where p.hoofd_sub = 'Hoofdpand' and p.dbk_object = ?", new MapListHandler(), id);
+
+        if(rows.isEmpty()) {
+            throw new IllegalArgumentException("DBK met ID " + id + " niet gevonden");
+        }
+
+        String vrhBagId = (String)rows.get(0).get("vrh_bag_id");
+        Map<String,Object> bagAdres = DB.bagQr().query("select openbareruimtenaam, huisnummer, huisletter, huisnummertoevoeging, postcode, woonplaatsnaam from bag_actueel.adres where nummeraanduiding = ?", new MapHandler(), vrhBagId);
+
+        Map<String,Object> row = rows.get(0);
+
+        if(bagAdres != null) {
+            row.put("straatnaam", bagAdres.get("openbareruimtenaam"));
+            row.put("huisnummer", bagAdres.get("huisnummer"));
+            row.put("huisletter", bagAdres.get("huisletter"));
+            row.put("toevoeging", bagAdres.get("huisnummertoevoeging"));
+            row.put("postcode", bagAdres.get("postcode"));
+            row.put("plaats", bagAdres.get("woonplaatsnaam"));
+            row.put("bagPunt", bagAdres.get("geopunt"));
+        }
+
+        return rowToJson(row, true, true);
     }
 
     public static JSONObject dbkJson(Connection c, int id) throws Exception {
