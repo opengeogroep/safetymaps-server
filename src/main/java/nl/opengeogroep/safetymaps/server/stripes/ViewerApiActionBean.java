2524356af639e14ffdf72464c97357d5ce17a3b9
mvdstruijk <m_struijk1990@hotmail.nl>
2019-11-13 10:36:07 +0100

    deleted duplicate line

--- src/main/java/nl/opengeogroep/safetymaps/server/stripes/ViewerApiActionBean.java
+++ src/main/java/nl/opengeogroep/safetymaps/server/stripes/ViewerApiActionBean.java
@@ -267,7 +267,6 @@ public class ViewerApiActionBean implements ActionBean {
             options.put("sourceVrhAGSAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(VrhAGSProxyActionBean.ROLE));
             options.put("sourceSafetyConnectAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(SafetyConnectProxyActionBean.ROLE));
             options.put("incidentMonitorAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(ROLE_INCIDENTMONITOR));
-            options.put("incidentMonitorAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(ROLE_INCIDENTMONITOR));
             options.put("incidentMonitorKladblokAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(ROLE_INCIDENTMONITOR_KLADBLOK));
             options.put("eigenVoertuignummerAuthorized", request.isUserInRole(ROLE_ADMIN) || request.isUserInRole(ROLE_EIGEN_VOERTUIGNUMMER));
 
