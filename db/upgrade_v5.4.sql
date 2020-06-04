805c992cb857b88cf3a0b163d681c55263709fa0
Matthijs <matthijsln@gmail.com>
2020-06-04 18:29:09 +0200

    Fix typo

--- db/upgrade_v5.4.sql
+++ db/upgrade_v5.4.sql
@@ -1,5 +1,5 @@
 SET search_path = safetymaps, pg_catalog;
 
-create table safetymaps.drawing(incident varchar, primary key, features text, last_modified timestamp, modified_by varchar);
+create table safetymaps.drawing(incident varchar primary key, features text, last_modified timestamp, modified_by varchar);
 
 
