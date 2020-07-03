3bbc50e79c9ebb602aea84da7ca804b128f85372
Bart Verhaar <b.verhaar@safetyct.com>
2020-07-03 12:22:57 +0200

    Added googleMapsNavigationAuthorized

--- db/upgrade_v5.4.sql
+++ db/upgrade_v5.4.sql
@@ -3,3 +3,5 @@ SET search_path = safetymaps, pg_catalog;
 create table safetymaps.drawing(incident varchar primary key, features text, last_modified timestamp, modified_by varchar);
 
 
+insert into role(role, protected, description) values
+('incident_googlemapsnavigation', true, 'Benodigd voor Google Maps navigatie icoon bij Incident Adres')
