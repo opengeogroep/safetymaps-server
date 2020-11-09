c6640e65356bafaadd36df6c4fafaba4066ec72f
Bart Verhaar <b.verhaar@safetyct.com>
2020-11-09 12:51:41 +0100

    Add role to db

--- db/upgrade_v5.4.sql
+++ db/upgrade_v5.4.sql
@@ -3,8 +3,11 @@ SET search_path = safetymaps, pg_catalog;
 create table safetymaps.drawing(incident varchar primary key, features text, last_modified timestamp, modified_by varchar);
 
 
-insert into role(role, protected, description) values
-('incident_googlemapsnavigation', true, 'Benodigd voor Google Maps navigatie icoon bij Incident Adres')
+insert into safetymaps.role(role, protected, description) values
+('incident_googlemapsnavigation', true, 'Benodigd voor Google Maps navigatie icoon bij Incident Adres');
 
-insert into role(role, protected, description) values
-('incidentmonitor_training', true, 'Benodigd om trainings incidenten vanuit Safety Connect ook te tonen')
+insert into safetymaps.role(role, protected, description) values
+('incidentmonitor_training', true, 'Benodigd om trainings incidenten vanuit Safety Connect ook te tonen');
+
+insert into safetymaps.role(role, protected, description) values
+('kro', true, 'Benodigd voor het laden van kro');
