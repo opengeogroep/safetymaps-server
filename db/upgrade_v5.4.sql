ff21c4524c3650dfe7fc0766ec5b8def1ed2ee6a
Bart Verhaar <b.verhaar@safetyct.com>
2021-02-18 09:56:31 +0100

    Fix error

--- db/upgrade_v5.4.sql
+++ db/upgrade_v5.4.sql
@@ -3,16 +3,16 @@ SET search_path = safetymaps, pg_catalog;
 create table safetymaps.drawing(incident varchar primary key, features text, last_modified timestamp, modified_by varchar);
 
 insert into role(role, protected, description) values
-('incident_googlemapsnavigation', true, 'Benodigd voor Google Maps navigatie icoon bij Incident Adres')
+('incident_googlemapsnavigation', true, 'Benodigd voor Google Maps navigatie icoon bij Incident Adres');
 
 insert into role(role, protected, description) values
-('incidentmonitor_training', true, 'Benodigd om trainings incidenten vanuit Safety Connect ook te tonen')
+('incidentmonitor_training', true, 'Benodigd om trainings incidenten vanuit Safety Connect ook te tonen');
 
 insert into role(role, protected, description) values
-('kladblokchat_editor', true, 'Benodigd voor het kunnen toevoegen van kladblokregels in de viewer')
+('kladblokchat_editor', true, 'Benodigd voor het kunnen toevoegen van kladblokregels in de viewer');
 
 insert into role(role, protected, description) values
-('kladblokchat_viewer', true, 'Benodigd voor het kunnen zien van toegevoegde kladblokregels vanuit de viewer')
+('kladblokchat_viewer', true, 'Benodigd voor het kunnen zien van toegevoegde kladblokregels vanuit de viewer');
 
 CREATE TABLE safetymaps.kladblok (
     id serial PRIMARY KEY,
