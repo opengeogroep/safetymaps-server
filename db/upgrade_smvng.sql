1fa4e20a9d2d759f144af1715fb7303b04a2f9ad
Bart Verhaar <b.verhaar@safetyct.com>
2021-10-04 09:42:35 +0200

    Add .sql file

---
+++ db/upgrade_smvng.sql
@@ -0,0 +1,41 @@
+insert into safetymaps.role(role, protected, description) 
+values('smvng_incident_showchat', true, 'SMVNG Incident - Toon chat kladblokregels');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_incident_addchat', true, 'SMVNG Incident - Voeg chat kladblokregels toe');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_incident_googlenav', true, 'SMVNG Incident - Toon Google Nav icon achter incidentadres');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_incidentmonitor_leaveincident', true, 'SMVNG Incidentmonitor - Toon knop om gekoppeld incident te verlaten');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_livedata_streetview', true, 'SMVNG Livedata - Toon Streetview');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_livedata_twitter', true, 'SMVNG Livedata - Toon Twitter');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_livedata_drawings', true, 'SMVNG Livedata - Toon tekenen');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_livedata_photos', true, 'SMVNG Livedata - Toon fotos');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_livedata_earth', true, 'SMVNG Livedata - Toon Google earth');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_vehicleinfo_incidentlocations', true, 'SMVNG Vehicleinfo - Toon voertuiglocaties bij een incident');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_vehicleinfo_maplocations', true, 'SMVNG Vehicleinfo - Toon alle voertuiglocaties op de kaart');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_vehicleinfo_unasigned', true, 'SMVNG Vehicleinfo - Toon ook de locaties van niet gekoppelde voertuigen');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_drawing_crud', true, 'SMVNG Drawing - CRUD acties');
+
+insert into safetymaps.role(role, protected, description) 
+values('smvng_imagedrawing', true, 'SMVNG Imagedrawing');
